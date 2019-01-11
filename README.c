ae9a3d48ba91ef56d151838f356b1e4b9d902c5a
arshad408 <arshadachu408@gmail.com>
2019-01-11 12:16:32 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
-#define MAXSIZE 10
- 
 void main()
 {
-    int array[MAXSIZE];
-    int i, j, num, temp;
- 
-    printf("Enter the value of num \n");
-    scanf("%d", &num);
-    printf("Enter the elements one by one \n");
-    for (i = 0; i < num; i++)
-    {
-        scanf("%d", &array[i]);
-    }
-    printf("Input array is \n");
-    for (i = 0; i < num; i++)
-    {
-        printf("%d\n", array[i]);
-    }
-    for (i = 0; i < num; i++)
-    {
-        for (j = 0; j < (num - i - 1); j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-    printf("Sorted array is...\n");
-    for (i = 0; i < num; i++)
-    {
-        printf("%d\n", array[i]);
-    }
+  int array[a];
+  int i, j, num, temp;
+  printf("Enter the value of num \n");
+  scanf("%d", &num);
+  printf("Enter the elements one by one \n");
+  for (i = 0; i < num; i++)
+ {
+ scanf("%d", &ai]);
+ }
+ printf("Input array is \n");
+ for (i = 0; i < num; i++)
+ {
+  printf("%d\n", a[i]);
+ }
+ for (i = 0; i < num; i++)
+{
+ for (j = 0; j < (num - i - 1); j++)
+ {
+  if (a[j] > a[j + 1])
+ {
+ temp = a[j];
+a[j] = a[j + 1];
+a[j + 1] = temp;
+ }
+ }
+ }
+ printf("Sorted array is...\n");
+ for (i = 0; i < num; i++)
+{
+ printf("%d\n", a[i]);
+}
 }
